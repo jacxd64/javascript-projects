972962c485967bb6a3cd6cad5181aaba23bdcf51
jacxd64 <70988180+jacxd64@users.noreply.github.com>
2024-03-28 13:37:25 -0500

    part 6 done

--- arrays/exercises/part-six-arrays.js
+++ arrays/exercises/part-six-arrays.js
@@ -2,10 +2,38 @@
 
 //1) Define and initialize the arrays specified in the exercise to hold the name, chemical symbol and mass for different elements.
 
+let element1 = ['hydrogen', 'H', 1.008];
+let element2 = ['helium', 'He', 4.003];
+let element26 = ['iron', 'Fe', 55.85];
+
 //2) Define the array 'table', and use 'push' to add each of the element arrays to it. Print 'table' to see its structure.
+
+let table = [];
+table.push(element1, element2, element26);
+console.log(table);
 
 //3) Use bracket notation to examine the difference between printing 'table' with one index vs. two indices (table[][]).
 
+console.log(table[2]);
+console.log(table[2][1]);
+
 //4) Using bracket notation and the table array, print the mass of element1, the name for element 2 and the symbol for element26.
 
+console.log(table[0][2]);
+console.log(table[1][0]);
+console.log(table[2][1]);
+
 //5) 'table' is an example of a 2-dimensional array. The first “level” contains the element arrays, and the second level holds the name/symbol/mass values. Experiment! Create a 3-dimensional array and print out one entry from each level in the array.
+
+let Arrayd1 = [];
+
+let d2a1 = 1, d2a2 = 2, d2a3 = 3;
+let d2a = [d2a1, d2a2, d2a3];
+
+let d2b1 = 2, d2b2 = 3, d2b3 = 4;
+let d2b = [d2b1, d2b2, d2b3];
+
+let d3b1 = 3, d3b2 = 4, d3b3 = 5;
+let d2c = [d3b1, d3b2, d3b3];
+
+console.log(Arrayd1);
